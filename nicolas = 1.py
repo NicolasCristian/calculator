61336e1fc8f99b522e3321d96db0692a2d4da05a
NicolasCristian <103538006+NicolasCristian@users.noreply.github.com>
2022-07-15 21:33:29 -0300

    Add files via upload

---
+++ nicolas = 1.py
@@ -0,0 +1,23 @@
+
+##input e a entrada, onde o user ira inserir a informacao
+##int e o valor decimal
+
+
+ent1 = int(input('digite um numero:  '))
+print (ent1)
+ent2 = int(input('agora digite outro:  '))
+print (ent2)
+
+soma = ent1 + ent2
+sub = ent1 - ent2
+mult = ent1 * ent2
+div = ent1 / ent2
+
+
+print ('a soma dos numeros e: %d'%(soma))
+
+print ('a subtracao dos numeros e: %d'%(sub))
+
+print ('a multiplicacao dos numeros e: %d'%(mult))
+
+print ('a divisao dos numeros e: %d'%(div))
